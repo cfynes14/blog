19414a5f3823d531538c8e393e1858c46b335792
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2021-06-30 16:19:44 +0100

    init commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Blog
+</title>
+<link href="/css/app.css" rel="stylesheet"> 
+</head>
+
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+
+    <a href="/">Go back</a>
+
+<script src='/js/app.js'></script>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Blog
+</title>
+<link href="/css/app.css" rel="stylesheet"> 
+</head>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus culpa nemo placeat labore aut laborum aspernatur ipsam ullam ea esse porro maiores a cupiditate quae quidem blanditiis, tenetur eius laudantium dicta commodi ratione amet. Maxime, iusto earum ea error architecto laboriosam incidunt temporibus commodi perspiciatis, porro minus iure eius quo.</p>
+    </article>
+
+    <article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus culpa nemo placeat labore aut laborum aspernatur ipsam ullam ea esse porro maiores a cupiditate quae quidem blanditiis, tenetur eius laudantium dicta commodi ratione amet. Maxime, iusto earum ea error architecto laboriosam incidunt temporibus commodi perspiciatis, porro minus iure eius quo.</p>
+    </article>
+
+    <article>
+    <a href="/posts/my-third-post"><h1>My Third Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus culpa nemo placeat labore aut laborum aspernatur ipsam ullam ea esse porro maiores a cupiditate quae quidem blanditiis, tenetur eius laudantium dicta commodi ratione amet. Maxime, iusto earum ea error architecto laboriosam incidunt temporibus commodi perspiciatis, porro minus iure eius quo.</p>
+    </article>
+
+<script src='/js/app.js'></script>
+</body>
+</html>
